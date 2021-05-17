b341ef3bc2765a5e0f82e485b3a0d6a66eedcb40
yizhenl3 <yizhenl3@illinois.edu>
2021-05-16 19:29:47 -0500

    Q21-28 complete

--- Q21-28.hs
+++ Q21-28.hs
@@ -16,14 +16,22 @@ range a b = [x | x <- [1..b] , x >= a]
 range' :: Enum a => a -> a -> [a]
 range' a b = [a..b]
 
--- Q23
-rnd_select :: [a] -> Int -> IO [a]
-
-
-
-
-
-
-
 -- Q26
---combinations :: Int -> [a] -> [[a]]
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations n xs = [ xs !! i : x | i <- [0..(length xs)-1] 
+                                  , x <- combinations (n-1) (drop (i+1) xs) ]
+
+-- Q27
+combination :: Int -> [a] -> [([a],[a])]
+combination 0 xs     = [([],xs)]
+combination n []     = []
+combination n (x:xs) = ts ++ ds
+  where
+    ts = [ (x:ys,zs) | (ys,zs) <- combination (n-1) xs ]
+    ds = [ (ys,x:zs) | (ys,zs) <- combination  n    xs ]
+
+group :: [Int] -> [a] ->[[[a]]]
+group [] _ = [[[]]]
+group (n:ns) xs = [ a:b | (a,re) <- combination n xs
+                          , b <- group ns re ]
